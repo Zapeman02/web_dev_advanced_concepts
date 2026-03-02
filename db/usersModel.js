d6df7fe1fd7ea267969b1b2d915c05c303175f60
fillevipp <nystromphilip@gmail.com>
2026-03-02 20:52:47 +0100

    created venuemodel to getAllUsers. Called in server.js with a get route and displays name password and email.

---
+++ db/usersModel.js
@@ -0,0 +1,14 @@
+const {client} = require('./db');
+
+const usersModel = {
+    getAllUsers : async function(){
+        try{
+            const res = await client.query('SELECT * FROM users')
+            return res.rows
+        }catch(err){
+            console.log(err)
+            throw err
+        }
+    }
+}
+module.exports = usersModel
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const {connectDB} = require('./db/db');
 const venueModel = require('./db/venueModel');
+const usersModel = require('./db/usersModel')
 //arvid
 //routes mellan client och server genom venue functioner.
 const PORT = 8080
@@ -17,11 +18,20 @@ app.get('/api/venues', async (req,res) => {
         res.status(500).json({message: 'could not get venues', error: err});
     }
 })
-
+//test to see only links
 app.get('/api/venues/links', async(req,res)=>{
     try{
         const venues = await venueModel.getLinks(req.query.url)
         res.json(venues)
+    }catch(err){
+        console.log(err)
+    }
+})
+//test to see mockuser
+app.get('/api/users', async (req,res)=>{
+    try{
+        const users = await usersModel.getAllUsers()
+        res.json(users)
     }catch(err){
         console.log(err)
     }
