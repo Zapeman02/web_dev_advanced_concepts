292b0ee5f7788858b529fd63948ce38e7cb74f65
Zapeman02 <Zapeman02@gmail.com>
2026-03-03 15:43:34 +0100

    edit structure for rendering dom

--- public/script.js
+++ public/script.js
@@ -1,17 +1,103 @@
 
-//fetch från våra egna apier, och html uppbyggnad
-//tim
-function fetchAllVenues(){
-    fetch('api/venues')
-    .then(res=>res.json())
-    .then(data=>{
-       createAllVenueItems(data)
-    })
-
+const pages = {
+    HOME: 'home',
+    LOGIN: 'login'
 }
-fetchAllVenues()
+let CURRENT_PAGE = pages.HOME
+
+// ================== MAIN ENTRY POINT ===================  
+async function createHtmlDom(){
+// act as main
+// build html dom with header then all venues and then footer
+    try{
+        createHeader();
+        renderMain();
+
+        // menu bar jkpgcity, login
+        // search / filter options
+        // venueitems based on search
+        // footer
+        // if logged button is pressed show login screen
+        // default show home with venues
+
+    }catch(err){
+        console.log('Error building DOM:', err)
+    }
+}
+
+createHtmlDom()
+
+// ============ NAVIGATION =======================
+async function navigateTo(page){
+    CURRENT_PAGE = page;
+    clearMain();
+    renderMain();
+}
+
+// =============== PAGE LOADERS =======================
+
+async function renderMain(){
+    switch(CURRENT_PAGE){
+    case pages.HOME:
+        await loadHome();
+        break;
+    case pages.LOGIN:
+        loadLogin();
+        break;
+    default:
+        await loadHome();
+    }
+}
+
+function clearMain(){
+    const main = document.getElementById('main');
+    main.innerHTML = ''
+}
+
+async function loadHome() {
+    const venues = await fetchAllVenues();
+    createAllVenueItems(venues);
+}
+
+function loadLogin(){
+    const div = document.createElement('div');
+    div.innerText = 'Go Home';
+    div.addEventListener('click', () => {
+        navigateTo(pages.HOME);
+    })
+    const main = document.getElementById('main');
+    main.appendChild(div);
+}
+
+function createHeader(){
+    const div = document.createElement('div');
+    div.innerText = 'This is the header';
+    const header = document.getElementById('header');
+    header.appendChild(div)
+
+    const button = document.createElement('button')
+    button.innerText = 'Login'
+    button.addEventListener('click', () => {
+        navigateTo(pages.LOGIN)
+    })
+    header.appendChild(button)
+}
 
 
+//================== FETCH FUNCTIONS ======================
+//fetch från våra egna apier, och html uppbyggnad
+// funktionerna ska dubbelchecka input innan request
+// throw new Error('beskriva vad som hänt') om fel
+// Returnera jsonobject
+//tim
+async function fetchAllVenues(){
+    const res = await fetch('api/venues')
+    if(!res.ok) throw new Error('Failed to fetch venues')
+    return res.json()
+}
+
+
+/*
 function fetchUser(name){
     fetch('api/users/' + name)
     .then(res => {
@@ -19,48 +105,56 @@ function fetchUser(name){
         return res.json();
     })
     .then(data => {
-        console.log("Data from db," + data)
-    })
+        console.log("Data from db," + data);
+    });
 }
-//fetchUser('testadmin')
+//fetchUser('testadmin');*/
 
 
 
-
+// ================== DOM BUILDERS =====================
 //Kasper HTML moduler med js
 
 function createAllVenueItems(venue){
-    const main = document.getElementById("main")
-    const container = document.createElement("div")
-    container.classList.add("venueItemContainer")
-    main.appendChild(container)
-    
+    const main = document.getElementById("main");
+    const container = document.createElement("div");
+    container.classList.add("venueItemContainer");
+    main.appendChild(container);
+
     venue.forEach(venue =>{
-            createVenueItem(container, venue)
+            createVenueItem(container, venue);
         });
 }
 
 function createVenueItem(container, venue){
-    const venueItem = document.createElement("div")
-    venueItem.id = `venueItem_${venue.id}`
-    venueItem.classList = `venueItem`
+    const venueItem = document.createElement("div");
+    venueItem.id = `venueItem_${venue.id}`;
+    venueItem.classList = `venueItem`;
 
     if(venue.url){
-        venueItem.addEventListener('click', e => {
-            window.location.href = venue.url
-        })
-        venueItem.setAttribute('style', 'cursor: pointer;')
+        venueItem.addEventListener('click', () => handleVenueClick(venue.url));
+        venueItem.setAttribute('style', 'cursor: pointer;');
     }
 
-    const nameP = document.createElement("p")
-    nameP.innerText = venue.name
+    const nameP = document.createElement("p");
+    nameP.innerText = venue.name;
 
-    const districtP = document.createElement("p")
-    districtP.innerText = venue.district
+    const districtP = document.createElement("p");
+    districtP.innerText = venue.district;
 
-    venueItem.appendChild(nameP)
-    venueItem.appendChild(districtP)
+    venueItem.appendChild(nameP);
+    venueItem.appendChild(districtP);
 
-    container.appendChild(venueItem)
+    container.appendChild(venueItem);
 
 }
+
+function handleVenueClick(url){
+    if(!url.startsWith('http://') && !url.startsWith('https://')){
+        url = 'https://'+url;
+    }
+    window.location.href = url;
+}
+
+
+
